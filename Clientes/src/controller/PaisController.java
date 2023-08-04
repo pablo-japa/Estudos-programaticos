85d84f51b91918093191934fce4c762ea0bca97b
Pablo Massao Lins Bunno <regigigaspower2@gmail.com>
2023-08-04 10:38:09 -0300

    Update PaisController.java

--- Clientes/src/controller/PaisController.java
+++ Clientes/src/controller/PaisController.java
@@ -8,22 +8,32 @@ import View.PaisView;
 public class PaisController {
 	private List<Pais> listaDePaises;
 	private PaisView paisView;
-	
+
 	public PaisController() {
 		listaDePaises = new ArrayList<>();
 		paisView = new PaisView();
 	}
-	
+
 	public void adicionarPais(String nome, String capital, int populacao) {
 		Pais pais = new Pais(nome, capital, populacao);
 		listaDePaises.add(pais);
 	}
+
 	public void exibirListaDePaises() {
 		paisView.exibirListaDePaises(listaDePaises);
-	
-	}
-	public void removerPais() {
-		listaDePaises.remove(0);
+
 	}
 
+	public void limparLista() {
+		listaDePaises.removeAll(listaDePaises);
+	}
+
+	public void removerPais(String nome) {
+		for (Pais pais : listaDePaises) {
+			if (pais.getNome().equalsIgnoreCase(nome)) {
+				listaDePaises.remove(pais);
+				break;
+			}
+		}
+	}
 }
