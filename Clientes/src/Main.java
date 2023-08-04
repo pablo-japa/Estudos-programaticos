58d6bc58d85e2c8cae6f3b62ff8f398e0d2c6c1e
Pablo Massao Lins Bunno <regigigaspower2@gmail.com>
2023-08-04 10:37:25 -0300

    Update Main.java

--- Clientes/src/Main.java
+++ Clientes/src/Main.java
@@ -7,48 +7,53 @@ public class Main {
 		// TODO Auto-generated method stub
 		PaisController paisController = new PaisController();
 		Scanner scanner = new Scanner(System.in);
-		
+
 		int opcao = -1;
 		while (opcao != 0) {
-			 System.out.println("==== Menu ====");
-	            System.out.println("1. Adicionar país");
-	            System.out.println("2. Exibir lista de países");
-	            System.out.println("3. Remover País");
-	            System.out.println("4. Limpar Lista");
-	            System.out.println("0. Sair");
-	            System.out.print("Escolha uma opção: ");
-	            opcao = scanner.nextInt();
-	            
-	            	
-	            	switch(opcao) {
-	            	case 1:
-	            		System.out.println("Digite o Nome do País: ");
-	            		scanner.nextLine();
-	            		String nome = scanner.nextLine();
-	            		
-	            		System.out.println("Digite a capital do País: " + nome + ":");
-	            		String capital = scanner.nextLine();
-	            		
-	            		System.out.println("Digite a população do País: ");
-	            		int populacao = scanner.nextInt();
-	            		
-	            	paisController.adicionarPais(nome, capital, populacao);
-	            	break;
-	            	case 2:
-	                    paisController.exibirListaDePaises();
-	                    break;
-	            	case 3:
-	            		System.out.println("Qual Pais Deseja remover?");
-	            		paisController.removerPais();
-	            		break;
-	            	case 0:
-	            		System.out.println("Saindo...");
-	            		break;
-	            	default:
-	            		System.out.println("Opção invalida");
-	            		break;
-	            	}
-			
+			System.out.println("==== Menu ====");
+			System.out.println("1. Adicionar país");
+			System.out.println("2. Exibir lista de países");
+			System.out.println("3. Limpar Lista");
+			System.out.println("4. remover um País");
+			System.out.println("0. Sair");
+			System.out.print("Escolha uma opção: ");
+			opcao = scanner.nextInt();
+
+			switch (opcao) {
+			case 1:
+				System.out.println("Digite o Nome do País: ");
+				scanner.nextLine();
+				String nome = scanner.nextLine();
+
+				System.out.println("Digite a capital do País: " + nome + ":");
+				String capital = scanner.nextLine();
+
+				System.out.println("Digite a população do País: ");
+				int populacao = scanner.nextInt();
+
+				paisController.adicionarPais(nome, capital, populacao);
+				break;
+			case 2:
+				paisController.exibirListaDePaises();
+				break;
+			case 3:
+				System.out.println("Limpando lista...");
+				paisController.limparLista();
+				break;
+			case 4:
+				System.out.println("deseja remover qual País?");
+				scanner.nextLine();
+				nome = scanner.nextLine();
+				paisController.removerPais(nome);
+				break;
+			case 0:
+				System.out.println("Saindo...");
+				break;
+			default:
+				System.out.println("Opção invalida");
+				break;
+			}
+
 		}
 		scanner.close();
 	}
